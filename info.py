18a53e8dd5995384932b28a77bd6b124c29b8fd5
Amitsaka123 <asexplanation24@gmail.com>
2024-10-26 00:22:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movierequest56bot)
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movierequest56bot')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/ott_up_date')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/movierequest56bot')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'movierequest56bot') # Support Chat Link Without https:// or @
