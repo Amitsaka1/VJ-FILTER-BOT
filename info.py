fb7ad278e51a3b7eee7ffcfc98dde001f7ac4bf0
Amitsaka7784 <exchangedelta43@gmail.com>
2024-11-01 22:25:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://morethanmore:morethanmore@morethanmore.41bgc.mongodb.net/?retryWrites=true&w=majority&appName=morethanmore")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Razith7866:Razith786@cluster0.rpema.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "morethanmore")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'viralcollection')
 
